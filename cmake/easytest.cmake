ef5170e7439fccb720a57a6417e7e45461f8f276
Alexander Haase <alexander.haase@rwth-aachen.de>
2017-02-17 06:22:28 +0100

    Fixed used CMake functions.
    Some functions are not available in CMake 2.8.11. They have been replaced by
    ones that are available.

--- cmake/easytest.cmake
+++ cmake/easytest.cmake
@@ -59,7 +59,8 @@ macro (easytest_hook_post_compile TARGET CONFIG)
 	endif()
 
 	if (EASYTEST_COMPILE_FLAGS)
-		target_compile_options(${TARGET} PRIVATE ${EASYTEST_COMPILE_FLAGS})
+		set_property(TARGET ${TARGET} APPEND_STRING
+		             PROPERTY COMPILE_FLAGS "${EASYTEST_COMPILE_FLAGS}")
 	endif ()
 
 	if (EASYTEST_LINK)
@@ -175,11 +176,6 @@ macro (easytest_get_common_keys FILE)
 	# COMPILE_INCLUDES may be a space delimited string.
 	string(REPLACE " " ";" EASYTEST_COMPILE_INCLUDES
 	               "${EASYTEST_COMPILE_INCLUDES}")
-
-	# Postprocess COMPILE_FLAGS key, as it must be a list, but COMPILE_FLAGS may
-	# be a space delimited string.
-	string(REPLACE " " ";" EASYTEST_COMPILE_FLAGS
-	               "${EASYTEST_COMPILE_FLAGS}")
 endmacro ()
 
 
