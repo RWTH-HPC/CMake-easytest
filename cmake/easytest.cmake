fc25dfd5bf1cde7c56de2e59f2c565c38f6a9781
Sebastian Grabowski <sebastian@grabel.de>
2023-09-14 14:49:53 +0200

    Fix missed version specifier (#5)
    This is a followup of #4.
    
    Co-authored-by: Sebastian Grabowski <grabowski@itc.rwth-aachen.de>

--- cmake/easytest.cmake
+++ cmake/easytest.cmake
@@ -8,7 +8,7 @@
 #
 
 # Set the minimum required CMake version.
-cmake_minimum_required(VERSION 2.8.12)
+cmake_minimum_required(VERSION 3.13.4...3.27.4)
 
 
 # The following keys will be searched in the tests main source file. Tests may
